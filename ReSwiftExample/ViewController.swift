04d995b1eb08bf24e4e975c2dca53dbb33bfc128
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2018-02-10 22:43:38 +0900

    とりあえずセル表示

--- ReSwiftExample/ViewController.swift
+++ ReSwiftExample/ViewController.swift
@@ -7,20 +7,28 @@
 //
 
 import UIKit
-import ReSwift
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        cell.textLabel?.text = "cell \(indexPath.row)"
+        
+        return cell
+    }
+    
+}
