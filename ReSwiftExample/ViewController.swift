23a3ca670b181e4154f65a9dc5c75058fec2557a
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2018-02-10 22:36:00 +0900

    create xcode project

---
+++ ReSwiftExample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ReSwiftExample
+//
+//  Created by KojimaTatsuya on 2018/02/10.
+//  Copyright © 2018年 KojimaTatsuya. All rights reserved.
+//
+
+import UIKit
+import ReSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
