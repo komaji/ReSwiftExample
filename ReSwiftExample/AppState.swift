17c286efb9497a52bab00e86155929648d3e502f
Tatsuya Kojima <cy11121@shibaura-it.ac.jp>
2018-02-11 01:01:06 +0900

    State 作ってみた

---
+++ ReSwiftExample/AppState.swift
@@ -0,0 +1,16 @@
+//
+//  AppState.swift
+//  ReSwiftExample
+//
+//  Created by KojimaTatsuya on 2018/02/11.
+//  Copyright © 2018年 KojimaTatsuya. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+
+struct AppState: StateType {
+    
+    var itemList = ItemListState()
+    
+}
---
+++ ReSwiftExample/ItemListState.swift
@@ -0,0 +1,15 @@
+//
+//  ItemListState.swift
+//  ReSwiftExample
+//
+//  Created by KojimaTatsuya on 2018/02/11.
+//  Copyright © 2018年 KojimaTatsuya. All rights reserved.
+//
+
+import Foundation
+
+struct ItemListState {
+    
+    var items: [String] = []
+    
+}
